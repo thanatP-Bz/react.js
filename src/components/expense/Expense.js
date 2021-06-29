f67315815fa5b571a2cc144b22d587bf48885588
Thanat <bztzn512@gmail.com>
2021-06-28 21:49:59 -0400

    AddCondition

--- src/components/expense/Expense.js
+++ src/components/expense/Expense.js
@@ -6,25 +6,37 @@ import "../expense/Expenses.css";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
 const Expense = (props) => {
-  const [filteredYear, setFilterYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const dropDownChangeHandler = (selectedYear) => {
-    setFilterYear(selectedYear);
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter
-          slected={filteredYear}
-          onChangeFilter={dropDownChangeHandler}
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
