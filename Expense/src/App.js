b7eb911767418d3e16ee987ef89b60c1892be6f6
Thanat <bztzn512@gmail.com>
2021-07-22 19:29:36 -0400

    replaceCode

---
+++ Expense/src/App.js
@@ -0,0 +1,50 @@
+import Expense from "./components/expense/Expense";
+import NewExpenses from "./NewExpense/NewExpenses";
+import React, { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: "e5",
+    title: "New Chair",
+    amount: 120,
+    date: new Date(2021, 5, 11),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div>
+      <NewExpenses onAddExpense={addExpenseHandler} />
+      <Expense items={expenses} />
+    </div>
+  );
+};
+
+export default App;
