9d872bd0547da1a67f4c22521ad2478283095e0b
Thanat <bztzn512@gmail.com>
2021-07-27 18:13:31 -0400

    classComponents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from "react";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div>
+    this.state = { lat: null };
 
-    </div>
-  );
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //we call set state
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+  //React we have to define render
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
 }
 
 export default App;
---
+++ src/Season.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Season = () => {
+  return <div></div>;
+};
+
+export default Season;
