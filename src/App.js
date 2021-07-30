25897ed034c61bc57852611c99c25e0b8a625122
Thanat <bztzn512@gmail.com>
2021-07-30 15:00:36 -0400

    axiosAPI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import SearchBar from "./Components/SearchBar";
 import React, { Component } from "react";
+import axios from "axios";
+import SearchBar from "./Components/SearchBar";
 
 export class App extends Component {
-  onSearchSubmit = (term) => {
-    console.log(term);
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get(
+      "https://api.unsplash.com//search/photos",
+      {
+        params: {
+          query: term,
+        },
+        headers: {
+          Authorization:
+            "Client-ID CY9v9ZAvQLwyIKVP5vNL0pL_yZ7fEnPAe1hl_H0rI4U",
+        },
+      }
+    );
+    console.log(response.data.results);
   };
+
   render() {
     return (
       <div>
