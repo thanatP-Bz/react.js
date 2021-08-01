b3dc6db8787a3be2248ead252d42e40fd1ba7625
Thanat <bztzn512@gmail.com>
2021-08-01 19:33:06 -0400

    APIyoutubeApp

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import YouTube from "./API/YouTube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 export class App extends Component {
-  onTermSubmit = (term) => {
-    YouTube.get("/search", {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("");
+  }
+
+  onTermSubmit = async (term) => {
+    const response = await YouTube.get("/search", {
       params: {
         q: term,
       },
     });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "20px" }}>
         <SearchBar onSubmit={this.onTermSubmit} />
+        <div className="ui stackable two column grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
