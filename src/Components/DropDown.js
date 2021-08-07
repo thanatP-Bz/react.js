1052baf3f8c1e4ad90abcc34376e6c8af33e55ef
Thanat <bztzn512@gmail.com>
2021-08-06 20:53:54 -0400

    dropdown

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-const DropDown = (props) => {
-  return <div>{props.options}</div>;
+const DropDown = ({ options }) => {
+  const renderOptions = options.map((option) => {
+    return <div key={option.value}></div>;
+  });
+
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select the color</label>
+        <div className="ui selcection dropdown visible active">
+          <i className="dropdown icon"></i>
+          <div className="text">Select Color</div>
+          <div className="menu visible transition">{renderOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DropDown;
