73966b6f6c4797e85c60491ac6b029370977a1f0
Thanat <bztzn512@gmail.com>
2021-08-29 21:31:19 -0400

    navbarAdded

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
+import Button from "./Button";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
@@ -25,7 +26,35 @@ const Navbar = () => {
                   Home
                 </Link>
               </li>
+              <li className="nav-item">
+                <Link
+                  to="/services"
+                  className="nav-link"
+                  onClick={closeMobileMenu}
+                >
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/products"
+                  className="nav-link"
+                  onClick={closeMobileMenu}
+                >
+                  Products
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/sign-up"
+                  className="nav-link"
+                  onClick={closeMobileMenu}
+                >
+                  Sign Up
+                </Link>
+              </li>
             </ul>
+            {button && <Button>SIHN UP</Button>}
           </div>
         </div>
       </nav>
