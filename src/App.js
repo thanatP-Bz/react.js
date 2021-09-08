a7532e209333d79d6e79fadec08f2a6ec829e9ec
Thanat <bztzn512@gmail.com>
2021-09-07 22:12:25 -0400

    PizzaWebsite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Menu from "./components/Menu";
-import About from "./pages/About.js";
-import Footer from "./components/Footer";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+
 
 function App() {
   return (
-    <div className="app">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/menu" exact component={Menu} />
-          <Route path="/about" exact component={About} />
-          <Route path="/contact" exact component={Contact} />
-        </Switch>
-        <Footer />
-      </Router>
+    <div>
+
     </div>
   );
 }
